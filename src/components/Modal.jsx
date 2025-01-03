972ea9207debfef14dc085f51d9772134d6a9acd
Eric Lee <ericlee@Erics-MacBook-Pro-142.local>
2025-01-02 21:48:38 -0800

    Start

---
+++ src/components/Modal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+//lets people see moves/abilities
+function Modal() {
+  return (
+    <></>
+  )
+}
+
+export default Modal
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { first151Pokemon, getFullPokedexNumber } from "../utils";
+
+function SideNav() {
+  return (
+    <nav>
+      <div className={"header"}>
+        <h1 className="text-gradient">Pokédex</h1>
+      </div>
+      <input />
+      {first151Pokemon.map((pokemon, pokemonIndex) => {
+        return (
+          <button key={pokemonIndex} className={"nav-card"}>
+            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+            <p>{pokemon}</p>
+          </button>
+        );
+      })}
+    </nav>
+  );
+}
+
+export default SideNav;
